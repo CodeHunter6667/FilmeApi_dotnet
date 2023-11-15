308bafacd3b0089b27f75d1d244de84ae4231437
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-15 18:25:48 -0300

    Feito validacoes na classe Filme

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,9 +1,17 @@
-﻿namespace FilmesApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O titulo do filme é obrigatorio")]
+        [MaxLength(50, ErrorMessage = "O titulo do filme deve ter no maximo 50 caracteres")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O genero do filme é obrigatorio")]
+        [MaxLength(50, ErrorMessage = "O genero deve ter no maximo 50 caracteres")]
         public string Genero { get; set; }
+        [Required(ErrorMessage = "A duracao do filme é obrigatorio")]
+        [Range(70, 600, ErrorMessage = "A duracao deve ser entre 70 e 600 minutos")]
         public int Duracao { get; set; }
     }
 }
