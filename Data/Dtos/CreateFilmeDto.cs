500da994560eec9c52c6d8d86a1359630b7d81fa
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-17 18:50:30 -0300

    Criado dtos para transferencia de dados

---
+++ Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        [Required(ErrorMessage = "O titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O genero do filme é obriigatorio")]
+        [StringLength(50, ErrorMessage = "O tamanho do genero nao pode exceder 50 caracteres")]
+        public string  Genero { get; set; }
+        [Required]
+        [Range(70, 600, ErrorMessage = "A duracao deve ter entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
