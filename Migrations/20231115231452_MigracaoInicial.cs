631a933f3c3366e6d5c5a2b80c5939fe0bfa85fe
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-17 18:50:54 -0300

    Realizada migrations no bannco de dados

---
+++ Migrations/20231115231452_MigracaoInicial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FilmesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_filmes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Titulo = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Genero = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Duracao = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_filmes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_filmes");
+        }
+    }
+}
