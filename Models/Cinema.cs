6fdc5becdcb6b66ed3601f5a62fd328252747fd4
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-17 19:30:17 -0300

    Criado a entidade Cinema

---
+++ Models/Cinema.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Cinema
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do cinema é obrigatorio")]
+    public string Nome { get; set; }
+}
