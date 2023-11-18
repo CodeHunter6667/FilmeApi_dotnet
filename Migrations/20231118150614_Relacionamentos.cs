8a0d59161dfbcd91e63e6cddcb36421cb175580b
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-18 12:09:25 -0300

    Feito migracao para efetivar relacionamento

---
+++ Migrations/20231118150614_Relacionamentos.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FilmesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Relacionamentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_enderecos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Logradouro = table.Column<string>(type: "text", nullable: false),
+                    Numero = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_enderecos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_cinemas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: false),
+                    EnderecoId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_cinemas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_cinemas_tb_enderecos_EnderecoId",
+                        column: x => x.EnderecoId,
+                        principalTable: "tb_enderecos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_cinemas_EnderecoId",
+                table: "tb_cinemas",
+                column: "EnderecoId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_cinemas");
+
+            migrationBuilder.DropTable(
+                name: "tb_enderecos");
+        }
+    }
+}
