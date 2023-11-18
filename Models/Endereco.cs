5fb26fd9ac39a85a518448865d8aca9d7b2a2b5d
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-18 11:47:32 -0300

    Criada a entidade Endereco

---
+++ Models/Endereco.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Endereco
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O campo Logradouro é obrigatorio")]
+    public string Logradouro { get; set; }
+    [Required(ErrorMessage = "O campo Numero é obrigatorio")]
+    public int Numero { get; set; }
+}
