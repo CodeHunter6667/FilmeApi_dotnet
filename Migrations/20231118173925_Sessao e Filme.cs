e28c40cc0efc8b5c25fff9dbec7c4980fe1680dc
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-18 15:41:06 -0300

    Realizada Migracao da entidade Sessao

---
+++ Migrations/20231118173925_Sessao e Filme.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FilmesApi.Migrations
+{
+    public partial class SessaoeFilme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_sessoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FilmeId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_sessoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_sessoes_tb_filmes_FilmeId",
+                        column: x => x.FilmeId,
+                        principalTable: "tb_filmes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_sessoes_FilmeId",
+                table: "tb_sessoes",
+                column: "FilmeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_sessoes");
+        }
+    }
+}
---
+++ Migrations/20231118181322_Sessao e Cinema.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesApi.Migrations
+{
+    public partial class SessaoeCinema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CinemaId",
+                table: "tb_sessoes",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_sessoes_CinemaId",
+                table: "tb_sessoes",
+                column: "CinemaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_sessoes_tb_cinemas_CinemaId",
+                table: "tb_sessoes",
+                column: "CinemaId",
+                principalTable: "tb_cinemas",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_sessoes_tb_cinemas_CinemaId",
+                table: "tb_sessoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tb_sessoes_CinemaId",
+                table: "tb_sessoes");
+
+            migrationBuilder.DropColumn(
+                name: "CinemaId",
+                table: "tb_sessoes");
+        }
+    }
+}
