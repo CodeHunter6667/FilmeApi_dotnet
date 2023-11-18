ee3c04608f9821a79306cb9a3b76e80ea0040788
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-18 12:05:43 -0300

    Adicionado relacionamento nos dtos

--- Data/Dtos/CreateCinemaDto.cs
+++ Data/Dtos/CreateCinemaDto.cs
@@ -6,5 +6,6 @@ namespace FilmesApi.Data.Dtos
     {
         [Required(ErrorMessage = "O campo de nome é obrigatorio")]
         public string Nome { get; set; }
+        public int EnderecoId { get; set; }
     }
 }
