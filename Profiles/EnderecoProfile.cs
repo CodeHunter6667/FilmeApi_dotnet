d7e8e187ae9c8df6368c2d21c44b8b3721185e62
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-18 11:48:47 -0300

    Criado profile de endereco para o AutoMapper

---
+++ Profiles/EnderecoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles;
+
+public class EnderecoProfile : Profile
+{
+    public EnderecoProfile()
+    {
+        CreateMap<CreateEnderecoDto, Endereco>();
+        CreateMap<UpdateCinemaDto, Endereco>().ReverseMap();
+        CreateMap<Endereco, ReadCinemaDto>();
+    }
+}
