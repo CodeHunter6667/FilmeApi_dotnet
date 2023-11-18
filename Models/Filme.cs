3c6fcdf6d61817f733fb0067175f792b2ac10b24
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-18 15:40:17 -0300

    CRiado entidade Sessao e feito suas devidas relacoes

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -18,5 +18,6 @@ namespace FilmesApi.Models
         [Required(ErrorMessage = "A duracao do filme é obrigatorio")]
         [Range(70, 600, ErrorMessage = "A duracao deve ser entre 70 e 600 minutos")]
         public int Duracao { get; set; }
+        public virtual ICollection<Sessao> Sessoes { get; set; }
     }
 }
---
+++ Models/Sessao.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FilmesApi.Models;
+
+[Table("tb_sessoes")]
+public class Sessao
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public long FilmeId { get; set; }
+    public virtual Filme Filme { get; set; }
+    public int? CinemaId { get; set; }
+    public virtual Cinema Cinema { get; set; }
+}
