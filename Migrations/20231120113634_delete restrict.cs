bdd9534f13d3843ac26613ecd46039c05c4a9642
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-20 09:47:01 -0300

    migracao para implementar mudanca do delete

---
+++ Migrations/20231120113634_delete restrict.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesApi.Migrations
+{
+    public partial class deleterestrict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_cinemas_tb_enderecos_EnderecoId",
+                table: "tb_cinemas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_cinemas_tb_enderecos_EnderecoId",
+                table: "tb_cinemas",
+                column: "EnderecoId",
+                principalTable: "tb_enderecos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tb_cinemas_tb_enderecos_EnderecoId",
+                table: "tb_cinemas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tb_cinemas_tb_enderecos_EnderecoId",
+                table: "tb_cinemas",
+                column: "EnderecoId",
+                principalTable: "tb_enderecos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
