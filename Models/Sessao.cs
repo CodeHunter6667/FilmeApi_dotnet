a1b90d437ab220bba159e644e8311fad245858b2
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-20 08:28:18 -0300

    Adequacao da classe para padrao muitos-para-muitos

--- Models/Sessao.cs
+++ Models/Sessao.cs
@@ -6,11 +6,7 @@ namespace FilmesApi.Models;
 [Table("tb_sessoes")]
 public class Sessao
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-    [Required]
-    public long FilmeId { get; set; }
+    public int FilmeId { get; set; }
     public virtual Filme Filme { get; set; }
     public int? CinemaId { get; set; }
     public virtual Cinema Cinema { get; set; }
